c022057eeae75870d14324dee3575630de3208e4
YuHang-Diao <951900584@qq.com>
2023-01-07 22:11:10 +0800

    脚本

---
+++ build.sh
@@ -0,0 +1,6 @@
+cd /opt/software/herostory
+mvn package
+docker rm -f game
+docker rmi -f diaoyuhang/herostory:1
+docker build -t diaoyuhang/herostory:1 .
+docker run -p 8080:8080 -d  --name game diaoyuhang/herostory:1
